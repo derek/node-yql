a601adf9ef5f0e3bb2830747ae8b3d6b8f8b0ead
Derek Gathright <derek@derek-gathrights-macbook-pro.local>
2010-03-06 01:40:41 -0800

    README update

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ var YQL = require('./yql');
 
 YQL.get("SELECT * FROM weather.forecast WHERE location=90066", function(response) {
 	
-	var location 	= response.query.results.channel.location,
+	var	location 	= response.query.results.channel.location,
 		condition 	= response.query.results.channel.item.condition;
 	
 	sys.puts("The current temperature in " + location.city + " is " + condition.temp + " degrees");
