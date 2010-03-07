9dc43c634e3a68512d5b921b79d1f9d36964161b
Derek Gathright <drgath@gmail.com>
2010-03-06 16:33:21 -0800

    realized node supported the encodeURIComponent function, so using that instead

--- yql.js
+++ yql.js
@@ -2,12 +2,9 @@
 var http = require("http");
 
 exports.get = function(yql, callback){
-
-	// Some pretty basic encoding.  Will need more testing
-	yql = yql.replace(/\s/g, "%20").replace(/\=/g, '%3D');
-
+	
 	var client 	= http.createClient(80, "query.yahooapis.com");
-	var url 	= "http://query.yahooapis.com/v1/public/yql?q=" + yql + "&format=json";
+	var url 	= "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(yql) + "&format=json";
 	var request = client.request("GET", url, {"host": "query.yahooapis.com", "User-Agent": "NodeJS HTTP Client"});
 
 	request.addListener('response', function (response) {
