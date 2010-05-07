d2997afe19bbb9431c7903c81dcecdad4fea047e
Derek Gathright <drgath@gmail.com>
2010-05-06 23:45:14 -0700

    YQL statements now support variable bindings.  Parameters are the same as the YUI3 gallery-yql module.

--- example.js
+++ example.js
@@ -1,24 +1,42 @@
-
 var	sys = require("sys"),
 	YQL = require('./yql');
 
-var zip	= 90066;
+// Test #1
+YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response, error) {
 
-YQL.exec("SELECT * FROM weather.forecast WHERE location=" + zip, function(response, error) {
-
-	if (!error) {
-	
+	if (error) {
+		sys.puts("Error: " + error);
+	} else {
 		var	location 	= response.query.results.channel.location,
 			condition 	= response.query.results.channel.item.condition;
-
-		sys.puts("The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
-	
-	} else {
-	
-		sys.puts("Error: " + error);
-	
+		sys.puts("Test #1... " + "The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
 	}
 
-});
+}, {"zip": 90066});
 
-sys.puts("Fetching weather...");
+
+// Test #2
+YQL.exec("SELECT * FROM weather.forecast WHERE (location = 66213)", function(response, error) {
+
+	if (error) {x
+		sys.puts("Error: " + error);
+	} else {
+		var	location 	= response.query.results.channel.location,
+			condition 	= response.query.results.channel.item.condition;
+		sys.puts("Test #2... " + "The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
+	}
+
+}, {}, {foo: "bar"});
+
+
+// Test #3
+YQL.exec("select * from twitter.user.timeline where (id = @id)", function(response, error) {
+
+	if (error) {
+		sys.puts("Error: " + error);
+	} else {	
+		var tweets = response.query.results.entry;
+		sys.puts("Test #3... " + tweets[0].title);
+	}
+
+}, {id:"derek"}, {https:true});
--- yql.js
+++ yql.js
@@ -1,32 +1,56 @@
-exports.exec = function (yql, callback, secure) {
+exports.exec = function (yql, callback, params, opts) {
 	
-	var	client, host, http, request;
+	var	http = require("http"), sys = require("sys"),
+		params = params || {},
+		opts = opts || {},
+		scheme = "http" + (typeof opts.https != "undefined" &&  opts.https.toString() == "true" ? "s" : ""),
+		host = "query.yahooapis.com",
+		port = 80,
+		path = "/v1/public/yql",
+		queryString = '?',
+		client, request, url;
+
+	// Attach some paramaters
+	params.format = 'json';
+	params.q = yql
+	if (!params.env) {
+		params.env = 'http:/'+'/datatables.org/alltables.env';
+	}
 	
-	http = require("http");
+	// Construct the query string
+	for(var key in params) {
+		queryString += key + "=" + encodeURIComponent(params[key]) + "&";
+	}
 	
-	host = "query.yahooapis.com";
-	client = http.createClient(80, host);
+	// Construct the URL
+	url = scheme + "://" + host + path + queryString;
+	
+	// Create the HTTP Client, and make the request
+	client = http.createClient(port, host);
 	request	= client.request(
 		"GET",
-		"http" + (secure ? "s" : "") + "://" + host + "/v1/public/yql?q=" + encodeURIComponent(yql) + "&format=json",
+		url,
 		{"host": host, "User-Agent": "A Node.js HTTP client"}
 	);
 
+	// Handle the response
 	request.addListener('response', function (response) {
+		
 		var responseBody = '';
 		
-		response.setBodyEncoding("utf8");
-		
+		// Combine the chunks of data as they return
 		response.addListener("data", function (chunk) {	
 			responseBody += chunk;
 		});
 		
+		// The response is complete
 		response.addListener("end", function () {
-			var r = JSON.parse(responseBody);
-			if (r.error) {
-				callback(r, r.error.description);
+			// Turn the JSON string into an object, then fire the callback
+			var resp = JSON.parse(responseBody);
+			if (resp.error) {
+				callback(resp, resp.error.description);
 			} else {
-				callback(r, false);
+				callback(resp, false);
 			}
 		});
 	});
