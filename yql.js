1600df772a0feb40ead223c2d1cda0aa0551a6f0
Derek Gathright <drgath@gmail.com>
2010-05-07 00:34:13 -0700

    Using the 3rd http.createClient() param for https instead of relying on a constructed URL, so the code is a little cleaner now.

--- yql.js
+++ yql.js
@@ -1,55 +1,53 @@
 exports.exec = function (yql, callback, params, opts) {
 	
-	var	http = require("http"), 
-		sys = require("sys"),
+	var	http = require("http"),
 		params = params || {},
 		opts = opts || {},
-		scheme = "http" + (typeof opts.https !== "undefined" && opts.https.toString() === "true" ? "s" : ""),
 		host = "query.yahooapis.com",
-		port = 80,
-		path = "/v1/public/yql",
-		queryString = '?',
-		client, request, url;
+		path = "/v1/public/yql?",
+		secure = (typeof opts.https !== "undefined" && opts.https.toString() === "true" ? true : false),
+		port = secure ? 443 : 80,
+		client, request;
 
-	// Attach some paramaters
+	// Add some of the required paramaters
 	params.format = 'json';
 	params.q = yql;
 	if (!params.env) {
 		params.env = 'http:/' + '/datatables.org/alltables.env';
 	}
 	
-	// Construct the query string
+	// Add the query string params onto the path
 	for (var key in params) {
-		queryString += key + "=" + encodeURIComponent(params[key]) + "&";
+		path += key + "=" + encodeURIComponent(params[key]) + "&";
 	}
 	
-	// Construct the URL
-	url = scheme + "://" + host + path + queryString;
-	
 	// Create the HTTP Client, and make the request
-	client = http.createClient(port, host); // TODO: If https, use 3rd param here instead of passing in the full URL to client.request
-	request	= client.request("GET", url, {"host": host, "User-Agent": "A Node.js HTTP client"});
+	client = http.createClient(port, host, secure);
+	request	= client.request("GET", path, {"host": host, "User-Agent": "A Node.js HTTP client"});
 
 	// Handle the response
 	request.addListener('response', function (response) {
 		
-		var responseBody = '';
+		var chunks = [];
 		
 		// Combine the chunks of data as they return
 		response.addListener("data", function (chunk) {	
-			responseBody += chunk;
+			chunks.push(chunk);
 		});
 		
 		// The response is complete
 		response.addListener("end", function () {
 			// Turn the JSON string into an object, then fire the callback
-			var resp = JSON.parse(responseBody);
-			if (resp.error) {
-				callback(resp, resp.error.description);
+			var jsonString = chunks.join(''),
+				result = JSON.parse(jsonString);
+				
+			if (result.error) {
+				callback(result, result.error.description);
 			} else {
-				callback(resp, false);
+				callback(result, false);
 			}
 		});
+		
 	});
 
 	request.end();
