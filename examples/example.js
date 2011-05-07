33332a4c73cee90bdbbbb6e02500fccc1e0288c2
Derek Gathright <drgath@gmail.com>
2011-05-07 15:36:39 -0700

    bump to 0.4.0.  Replaced native HTTP library with Request. Cleaned up code & package.json

--- examples/example.js
+++ examples/example.js
@@ -7,47 +7,47 @@ All rights reserved.
 
 */
 
-var	sys = require("sys"),
+var	sys = require('sys'),
 	YQL = require('yql');
-
 
 // Example #1
 new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response) {
 
 	if (response.error) {
-		sys.puts("Example #1... Error: " + response.error.description);
+		console.log("Example #1... Error: " + response.error.description);
 	} 
 	else {
-		var	location 	= response.query.results.channel.location,
-			condition 	= response.query.results.channel.item.condition;
-		sys.puts("Example #1... " + "The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
+		var	location  = response.query.results.channel.location,
+			condition = response.query.results.channel.item.condition;
+		console.log("Example #1... The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
 	}
 
 }, {"zip": 90066});
 
 
 // Example #2
+new YQL.exec("select * from twitter.user.timeline where (id = @id)", function(response) {
+
+	if (response.error) {
+	    console.log(require('util').inspect(error));
+		console.log("Example #2... Error: " + response.error.description);
+	} 
+	else {
+		var tweets = response.query.results.statuses.status;
+		console.log("Example #2... Latest tweet from @" + tweets[0].user.screen_name + ": " + tweets[0].text);
+	}
+
+}, {id:"derek"}, {ssl:true});
+
+
+// Example #3
 new YQL.exec("SELECT * FROM foobar.badTable WHERE (location = 66213)", function(response) {
 
 	if (response.error) {
-		sys.puts("Example #2... " + "Error: " + response.error.description);
+		console.log("Example #3... Error: " + response.error.description);
 	} 
 	else {
 		// Intentionally blank
 	}
-
-}, {}, {foo: "bar"});
-
-
-// Example #3
-new YQL.exec("select * from twitter.user.timeline where (id = @id)", function(response) {
-
-	if (response.error) {
-		sys.puts("Example #3... Error: " + response.error.description);
-	} 
-	else {
-		var tweets = response.query.results.statuses.status;
-		sys.puts("Example #3... Latest tweet from @" + tweets[0].user.screen_name + ": " + tweets[0].text);
-	}
-
-}, {id:"derek"}, {https:true});
+	
+}, false, {foo: "bar"});
