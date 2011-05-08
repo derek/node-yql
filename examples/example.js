0c91edfb74ec4a15636b64b95d71a0250c50d2b4
Derek Gathright <drgath@gmail.com>
2011-05-07 18:04:17 -0700

    Tabs & spacing are much happier now. *shakes fist at textmate*

--- examples/example.js
+++ examples/example.js
@@ -7,8 +7,8 @@ All rights reserved.
 
 */
 
-var	sys = require('sys'),
-	YQL = require('yql');
+var sys = require('sys'),
+    YQL = require('yql');
 
 // Example #1
 new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response) {
@@ -17,9 +17,9 @@ new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(
 		console.log("Example #1... Error: " + response.error.description);
 	} 
 	else {
-		var location  = response.query.results.channel.location,
-		    condition = response.query.results.channel.item.condition;
-		console.log("Example #1... The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
+        var location  = response.query.results.channel.location,
+            condition = response.query.results.channel.item.condition;
+        console.log("Example #1... The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
 	}
 
 }, {"zip": 90066});
@@ -28,14 +28,14 @@ new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(
 // Example #2
 new YQL.exec("select * from twitter.user.timeline where (id = @id)", function(response) {
 
-	if (response.error) {
+    if (response.error) {
         console.log(require('util').inspect(error));
         console.log("Example #2... Error: " + response.error.description);
-	} 
-	else {
-		var tweets = response.query.results.statuses.status;
-		console.log("Example #2... Latest tweet from @" + tweets[0].user.screen_name + ": " + tweets[0].text);
-	}
+    } 
+    else {
+        var tweets = response.query.results.statuses.status;
+        console.log("Example #2... Latest tweet from @" + tweets[0].user.screen_name + ": " + tweets[0].text);
+    }
 
 }, {id:"derek"}, {ssl:true});
 
@@ -43,11 +43,11 @@ new YQL.exec("select * from twitter.user.timeline where (id = @id)", function(re
 // Example #3
 new YQL.exec("SELECT * FROM foobar.badTable", function(response) {
 
-	if (response.error) {
+    if (response.error) {
         console.log("Example #3... Error: " + response.error.description);
-	} 
-	else {
-		// Intentionally blank
-	}
+    } 
+    else {
+        // Intentionally blank
+    }
 
 });
