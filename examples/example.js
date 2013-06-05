43846bd82d83670c50deca77da78668f515ae594
Derek Gathright <drgath@gmail.com>
2013-06-05 15:07:37 -0700

    Major refactoring.  Added unit tests.

--- examples/example.js
+++ examples/example.js
@@ -7,14 +7,18 @@ All rights reserved.
 
 */
 
-var YQL = require('../yql.js');
+var YQL = require('yql');
 
-var query = YQL("SELECT * FROM weather.forecast WHERE (location = @zip)", {ssl:true});
+// Example #1 - Param binding
+new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response) {
 
-query.exec({"zip": 94089}, function (error, results) {
-    if (error) return console.error(error);
-    
-    var location  = results.channel.location,
-        condition = results.channel.item.condition;
-    console.log("Example #1... The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
-});
+	if (response.error) {
+		console.log("Example #1... Error: " + response.error.description);
+	}
+	else {
+        var location  = response.query.results.channel.location,
+            condition = response.query.results.channel.item.condition;
+        console.log("Example #1... The current weather in " + location.city + ', ' + location.region + " is " + condition.temp + " degrees and " + condition.text);
+	}
+
+}, {"zip": 94089});
--- tests/index.js
+++ tests/index.js
@@ -7,103 +7,273 @@ All rights reserved.
 
 */
 
-var assert = require('assert'),
-    YQL    = require('../yql.js'),
-    query;
+var YQL = require('yql');
+var sinon = require('sinon');
+var expect = require('expect.js');
 
-// No YQL string
-try {
-    assert.throws(function () {
-        YQL(null);
-    }, /^Error: query must be a string$/);
-    pass(1);
-} catch (e) {
-    fail(1, e);
-}
+describe('YQL', function () {
+    describe('constructor', function () {
+        it('should return an instance', function () {
+            expect(new YQL('foo')).to.be.a(YQL);
+        });
+        it('should return an instance w/o new', function () {
+            expect(YQL('foo')).to.be.a(YQL);
+        });
 
-// Wrong options object
-try {
-    assert.throws(function () {
-        YQL("SHOW TABLES", null);
-    }, /^Error: options must be a object$/);
-    pass(2);
-} catch (e) {
-    fail(2, e);
-}
+        it('should throw an exception if missing a query', function () {
+            expect(function () {
+                new YQL();
+            }).to.throwError(function (e) {
+                expect(e).to.be.a(Error);
+            });
+        });
 
-// No param object
-try {
-    assert.throws(function () {
-        var query = YQL("SHOW TABLES");
-        query.exec(null);
-    }, /^Error: params must be a object$/);
-    pass(3);
-} catch (e) {
-    fail(3, e);
-}
+        it('should have a query property', function () {
+            var myQuery = 'SHOW TABLES';
+            expect(new YQL(myQuery).query).to.be.eql(myQuery);
+        });
 
-// No callback
-try {
-    assert.throws(function () {
-        var query = YQL("SHOW TABLES");
-        query.exec({}, null);
-    }, /^Error: callback must be a function$/);
-    pass(4);
-} catch (e) {
-    fail(4, e);
-}
+        it('should have a config property with defaults', function () {
+            expect(new YQL('SHOW TABLES').config).to.be.eql(YQL.DEFAULT_CONFIG);
+        });
 
-// Param binding
-query = YQL("SELECT * FROM weather.forecast WHERE (location = @zip)");
-query.exec({"zip": 90066}, function (error, results) {
-    try {
-        assert.ifError(error);
-        assert.ok(results);
-        pass(5);
-    } catch(e) {
-        fail(5, e);
-    }
+        it('should remember custom config properties', function () {
+            expect(new YQL('SHOW TABLES', {foo: 'bar'}).config).to.have.property('foo');
+        });
+    });
+
+    describe('#setParam', function () {
+        var yql = new YQL('SHOW TABLES');
+
+        it('should set a param', function () {
+            yql.setParam('foo', 'bar');
+            expect(yql.params).to.be.eql({foo: 'bar'});
+        });
+
+        it('should return itself', function () {
+            var value = yql.setParam('foo', 'bar');
+            expect(value).to.be(yql);
+        });
+    });
+
+    describe('#setParams', function () {
+        var yql = new YQL('SHOW TABLES');
+
+        it('should set multiple param', function () {
+            var params = {
+                foo: 'bar',
+                baz: 'bop'
+            };
+            yql.setParams(params);
+            expect(yql.params).to.be.eql(params);
+        });
+
+        it('should return itself', function () {
+            var value = yql.setParams({});
+            expect(value).to.be(yql);
+        });
+    });
+
+    describe('#setConfig', function () {
+        var yql = new YQL('SHOW TABLES');
+
+        it('should set a config', function () {
+            yql.setConfig('foo', 'bar');
+            expect(yql.config).to.have.property('foo');
+        });
+
+        it('should return itself', function () {
+            expect(yql.setConfig('foo', 'bar')).to.be(yql);
+        });
+    });
+
+    describe('#setConfigs', function () {
+        var yql = new YQL('SHOW TABLES');
+
+        it('should set multiple configs', function () {
+            yql.setConfigs({
+                foo: 'bar',
+                baz: 'bop'
+            });
+
+            expect(yql.config).to.have.property('foo');
+        });
+
+        it('should return itself', function () {
+            expect(yql.setConfigs({})).to.be(yql);
+        });
+    });
+
+    describe('#getURL', function () {
+        var yql = new YQL('SHOW TABLES');
+
+        it('should generate the correct HTTP URL', function () {
+            expect(yql.getURL()).to.be('http://query.yahooapis.com/v1/public/yql?format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&q=SHOW%20TABLES');
+        });
+
+        it('should generate the correct HTTPS URL', function () {
+            yql.setConfig('ssl', true);
+            expect(yql.getURL()).to.be('https://query.yahooapis.com/v1/public/yql?format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&q=SHOW%20TABLES');
+        });
+    });
+
+    describe('#exec', function () {
+
+        describe('without a response', function () {
+
+            var yql = new YQL('SHOW TABLES');
+
+            before(function () {
+                stub = sinon.stub(yql, '_httpRequest', function (config, callback) {
+                    callback();
+                })
+            });
+
+            after(function () {
+                stub.restore();
+            });
+
+            it('should throw an error', function () {
+                expect(yql.exec.bind(yql)).to.throwError(new RegExp(YQL.ERROR.missingBody));
+            });
+        });
+
+        describe('with an HTTP error', function () {
+
+            var yql = new YQL('SHOW TABLES');
+
+            before(function () {
+                stub = sinon.stub(yql, '_httpRequest', function (config, callback) {
+                    callback('This is an error message');
+                })
+            });
+
+            after(function () {
+                stub.restore();
+            });
+
+            it('should return an error', function (done) {
+                yql.exec(function (error) {
+                    expect(error).to.be('This is an error message');
+                    done();
+                });
+            });
+        });
+
+        describe('with a parsing error', function () {
+
+            var yql = new YQL('SHOW TABLES');
+
+            before(function () {
+                stub = sinon.stub(yql, '_httpRequest', function (config, callback) {
+                    callback(null, null, 'this is not valid json');
+                })
+            });
+
+            after(function () {
+                stub.restore();
+            });
+
+            it('should respond with an error', function (done) {
+                yql.exec(function (error, body) {
+                    expect(error).to.be.a(Error);
+                    expect(error.message).to.be('Unexpected token h');
+                    done();
+                });
+            });
+        });
+
+        describe('with a YQL error', function () {
+
+            var stub;
+
+            var yql = new YQL('SHOW TABLES');
+
+            var response = {
+                error: {
+                    description: 'This is a YQL error'
+                }
+            };
+
+            before(function () {
+                stub = sinon.stub(yql, '_httpRequest', function (config, callback) {
+                    var json = JSON.stringify(response);
+                    callback(null, null, json);
+                })
+            });
+
+            after(function () {
+                stub.restore();
+            });
+
+            it('should respond with an error', function (done) {
+                yql.exec(function (error, body) {
+                    expect(error).to.be.a(Error);
+                    expect(error.message).to.be(response.error.description);
+                    done();
+                });
+            });
+        });
+
+        describe('with a mocked response', function () {
+
+            var yql = new YQL('SHOW TABLES');
+
+            var response = {
+                query: {
+                    results: {
+                        tables: []
+                    }
+                }
+            };
+
+            yql._httpRequest = function (config, callback) {
+                var json = JSON.stringify(response);
+                callback(null, null, json);
+            };
+
+            it('should respond correctly', function (done) {
+                yql.exec(function (error, body) {
+                    expect(body).to.be.eql(response);
+                    done();
+                });
+            });
+        });
+    });
 });
 
-// Param binding + SSL
-query = YQL("SELECT * FROM html WHERE url = @url", {ssl: true});
-query.exec({url:"http://www.yahoo.com"}, function (error, results) {
-    try {
-        assert.ifError(error);
-        assert.ok(results);
-        pass(6);
-    } catch(e) {
-        fail(6, e);
-    }
-});
 
-// Non-existent table
-query = YQL("SELECT * FROM foobar.badTable");
-query.exec({}, function (error, results) {
-    try {
-        assert.ok(error);
-        assert.equal(results, null);
-        pass(7);
-    } catch(e) {
-        fail(7, e);
-    }
-});
+describe('YQL - Static', function () {
+    describe('#exec', function () {
+        var stub;
+        var response = {
+            query: {
+                results: {
+                    tables: []
+                }
+            }
+        };
 
-// Missing required fields
-query = YQL("SELECT * FROM html");
-query.exec({}, function (error, results) {
-    try {
-        assert.ok(error);
-        assert.equal(results, null);
-        pass(8);
-    } catch(e) {
-        fail(8, e);
-    }
-});
+        before(function () {
+            stub = sinon.stub(YQL.prototype, "_httpRequest", function (config, callback) {
+                var json = JSON.stringify(response);
+                callback(null, null, json);
+            });
+        });
 
-function pass(testID) {
-    console.log("Test #" + testID + " ... Passed");
-}
-function fail(testID, error) {
-    console.log("Test #" + testID + " ... FAIL", error);
-}
+        after(function () {
+            stub.restore();
+        })
+
+        it('should error if no query', function () {
+            expect(YQL.exec).withArgs().to.throwError(new RegExp(YQL.ERROR.invalidParameter + ': query'));
+        });
+
+        it('should return a response', function (done) {
+            YQL.exec('SHOW TABLES', function (results) {
+                expect(results).to.be.eql(response);
+                done()
+            }, {}, {});
+        });
+    });
+});
