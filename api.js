556ec631e50aa23daa21160c48d42e5807afe7aa
Derek Gathright <drgath@gmail.com>
2014-08-22 00:08:41 -0700

    v1.0.0 docs

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "YQL"
+    ],
+    "modules": [
+        "YQL"
+    ],
+    "allModules": [
+        {
+            "displayName": "YQL",
+            "name": "YQL",
+            "description": "YQL constructor"
+        }
+    ]
+} };
+});
